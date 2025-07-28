e1e6924bcde0adb8dd7404d8bedb2618b875b411
Matthew Beardwell <mbeardwell@users.noreply.github.com>
2025-07-28 19:48:38 +0100

    style: removed lines from README.md

--- src/build.sh
+++ src/build.sh
@@ -104,9 +104,7 @@ author=$(grep -Eo '\\author\{.*\}' "$LATEX" | sed 's/\\author{//; s/}//')
 	echo "<br/>"
 	echo "<br/>"
 	echo ""
-	echo "---"
 	cat "$TLDR"
-	echo "---"
 	echo ""
 } > "$HEADER_MD"
 
